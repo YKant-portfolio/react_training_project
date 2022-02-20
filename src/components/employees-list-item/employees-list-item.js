f523442402e9f9b038e83d786debd4a0172bffe6
Yuriy <ykantakuzin@bk.ru>
2022-02-20 15:12:29 +0300

    Оптимизация кода, есть 2 метода onToggleIcrease и onToggleLike выполняющие одну и туже функцию,  сделал метод который выполняет функционал 2 функций с использованием data-attribute

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,7 +1,7 @@
 import './employees-list-item.css';
 
 const EmployeesListItem = (props) => {
-	const { name, salary, onDelete, onToggleIncrease, onToggleLike, increase, like } = props;
+	const { name, salary, onDelete, onToggleProp, increase, like } = props;
 
 	let classNameIncrease = "list-group-item d-flex justify-content-between"
 	if (increase) {
@@ -14,13 +14,15 @@ const EmployeesListItem = (props) => {
 	return (
 		<li className={classNameIncrease}>
 			<span className="list-group-item-label"
-				onClick={onToggleLike}
+				onClick={onToggleProp}
+				data-toggle='like'
 			>{name}	</span>
 			<input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
 			<div className='d-flex justify-content-center align-items-center'>
 				<button type="button"
 					className="btn-cookie btn-sm"
-					onClick={onToggleIncrease}
+					onClick={onToggleProp}
+					data-toggle='increase'
 				>
 					<i className="fas fa-cookie"></i>
 				</button>
